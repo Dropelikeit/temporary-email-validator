8cd5e060f457cbb57caef52d9c2c1812acf8054c
Marcel Strahl <info@marcel-strahl.de>
2018-10-20 13:38:22 +0200

    change service provider for validator

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace MarcelStrahl\TemporaryValidator;
 
-use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use MarcelStrahl\TemporaryValidator\Rule\IsNotAnTemporaryEmailAddress;
 
 /**
@@ -15,16 +16,16 @@ class ServiceProvider extends BaseServiceProvider
 {
     public function boot(): void
     {
-        $this->loadTranslationsFrom(__DIR__.'/../translations', 'temporary-email-validator');
+        $this->loadTranslationsFrom(__DIR__ . '/../translations', 'temporary-email-validator');
 
         Validator::extend('not_temporary_email', function ($attribute, $value, $parameters, $validator) {
-            $rule = $this->app->get(IsNotAnTemporaryEmailAddress::class);
+            $rule = App::make(IsNotAnTemporaryEmailAddress::class);
             return $rule->passes($attribute, $value);
         });
     }
 
     public function register(): void
     {
-        //
+
     }
 }
