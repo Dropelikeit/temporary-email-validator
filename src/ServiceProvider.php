72562f7e696322b50ccee5b0539efc7089d4cb36
Marcel Strahl <info@marcel-strahl.de>
2021-09-27 14:56:11 +0200

    Added client interface binding to service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use MarcelStrahl\TemporaryValidator\Rule\IsNotAnTemporaryEmailAddress;
+use TemporaryEmailDetection\Client;
+use TemporaryEmailDetection\ClientInterface;
 
 /**
  * @author Marcel Strahl <info@marcel-strahl.de>
@@ -16,6 +18,11 @@ final class ServiceProvider extends BaseServiceProvider
     public function boot(): void
     {
         $this->loadTranslationsFrom(__DIR__ . '/../translations', 'temporary-email-validator');
+
+        $app = $this->app;
+        $this->app->bind(ClientInterface::class, static function () use ($app): ClientInterface {
+            return new Client($app->get(\GuzzleHttp\ClientInterface::class));
+        });
 
         Validator::extend('not_temporary_email', function ($attribute, $value, $parameters, $validator): bool {
             /** @var IsNotAnTemporaryEmailAddress $rule */
